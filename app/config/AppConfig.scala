600be41b3226cef3d7b0e997f70468e5833192dc
Mary1-5 <marymansha15@gmail.com>
2022-06-06 11:50:37 +0100

    adding urls to application.conf and appconfig files

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -41,4 +41,6 @@ class AppConfig @Inject()(config: Configuration, servicesConfig: ServicesConfig,
 
   val createCompileEventReportSummaryUrl: String = s"$ifURL${config.get[String](path = "serviceUrls.create-compile-event-report-summary")}"
 
+  def getErOverviewUrl: String = s"$ifURL${config.get[String](path = "serviceUrls.get-er-overview")}"
+
 }
