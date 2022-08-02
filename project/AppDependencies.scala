42f7795fa54b8798cc5b0b514dee8bbcf5a37c0c
cemyeniceri <cem.yeniceri@digital.hmrc.gov.uk>
2022-08-02 10:51:57 +0100

    PODS-7448 version is upgraded to avoid Potential logging of PII data

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,14 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.24.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.64.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.68.0",
     "com.github.java-json-tools" %% "json-schema-validator" % "2.2.14"
   )
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.24.0"              % "test, it",
     "com.github.simplyscala"  %% "scalatest-embedmongo"       % "0.2.4"               % Test,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.64.0"              % Test,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.68.0"              % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"              % "test, it",
     "org.mockito"             % "mockito-core"                % "4.0.0"               % "test",
     "org.mockito"             %% "mockito-scala"              % "1.16.42"             % "test",
