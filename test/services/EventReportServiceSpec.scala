eab48210ff5935390068fb847f103177e5454253
emrawsy <27211889+emrawsy@users.noreply.github.com>
2022-08-04 10:29:01 +0100

    PODS-7417 failing tests

--- test/services/EventReportServiceSpec.scala
+++ test/services/EventReportServiceSpec.scala
@@ -221,8 +221,18 @@ class EventReportServiceSpec extends AsyncWordSpec with Matchers with MockitoSug
         .thenReturn(Future.successful(responseJson))
       whenReady(eventReportService.getEvent(pstr, startDate, version, EventType.Event1)(implicitly, implicitly)) { result =>
         result mustBe responseJson
+
       }
     }
+
+    "return the payload from the connector when a valid event type is supplied for API1834" in {
+      when(mockEventReportConnector.getEvent(any(), any(), any(), any())(any(), any()))
+        .thenReturn(Future.successful(responseJson))
+      whenReady(eventReportService.getEvent(pstr, startDate, version, EventType.Event10)(implicitly, implicitly)) { result =>
+        result mustBe responseJson
+      }
+    }
+
     "return the payload from the connector when event type 20A" in {
       when(mockEventReportConnector.getEvent(any(), any(), any(), any())(any(), any()))
         .thenReturn(Future.successful(responseJson))
@@ -233,10 +243,10 @@ class EventReportServiceSpec extends AsyncWordSpec with Matchers with MockitoSug
 
     "return not found exception when an invalid event type is supplied" in {
       recoverToExceptionIf[NotFoundException] {
-        eventReportService.getEvent(pstr, startDate, version, EventType.Event20A)(implicitly, implicitly)
+        eventReportService.getEvent(pstr, startDate, version, EventType.Event22)
       } map {
         failure =>
-          failure.message mustBe "Not Found: ApiType not found for eventType (20A)"
+          failure.message mustBe "Not Found: ApiType not found for eventType (22)"
       }
     }
   }
@@ -348,6 +358,7 @@ class EventReportServiceSpec extends AsyncWordSpec with Matchers with MockitoSug
 }
 
 object EventReportServiceSpec {
+
   val responseJson: JsObject = Json.obj("event" -> "mockEvent - test passed")
   val pstr: String = "pstr"
   val createCompiledEventSummaryReportSchemaPath = "/resources.schemas/api-1826-create-compiled-event-summary-report-request-schema-v1.0.0.json"
