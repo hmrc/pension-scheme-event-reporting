8690862e14334c99d5dc08b10c12d8d0d37641a6
cemyeniceri <cem.yeniceri@digital.hmrc.gov.uk>
2022-11-08 17:57:02 +0000

    PODS-7731 dependency upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,23 +3,22 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"                   %% "bootstrap-backend-play-28"  % "7.8.0",
+    "uk.gov.hmrc"                   %% "bootstrap-backend-play-28"  % "7.11.0",
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-28"         % "0.73.0",
     "com.github.java-json-tools"    %  "json-schema-validator"      % "2.2.14",
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.13.4"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.8.0"                 % "test, it",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.11.0"                 % "test, it",
     "de.flapdoodle.embed"     %  "de.flapdoodle.embed.mongo"  % "3.5.1"                 % Test,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.73.0"                % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.62.2"                % "test, it",
-    "com.github.tomakehurst"  %  "wiremock"                   % "2.27.2"                % "test",
-    "com.github.tomakehurst"  %  "wiremock-jre8"              % "2.34.0"                % "test",
-    "org.scalatest"           %% "scalatest"                  % "3.2.14"                % "test",
-    "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0"                 % "test",
-    "org.scalatestplus"       %% "mockito-3-4"                % "3.2.10.0"              % "test",
-    "org.scalatestplus"       %% "scalacheck-1-15"            % "3.2.11.0"              % "test"
+    "com.github.tomakehurst"  %  "wiremock-jre8"              % "2.35.0"                % Test,
+    "org.scalatest"           %% "scalatest"                  % "3.2.14"                % Test,
+    "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0"                 % Test,
+    "org.scalatestplus"       %% "mockito-4-6"                % "3.2.14.0"              % Test,
+    "org.scalatestplus"       %% "scalacheck-1-17"            % "3.2.14.0"              % Test
   )
 
   def apply(): Seq[ModuleID] = compile ++ test
