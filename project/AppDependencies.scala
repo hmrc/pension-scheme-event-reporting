99507553772b6813d8ad7baed414b73d523de3ed
cemyeniceri <cem.yeniceri@digital.hmrc.gov.uk>
2022-12-09 11:32:01 +0000

    PODS-7860 api url fixed

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,12 +6,12 @@ object AppDependencies {
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-28"  % "7.11.0",
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-28"         % "0.73.0",
     "com.github.java-json-tools"    %  "json-schema-validator"      % "2.2.14",
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.13.4"
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.14.1"
   )
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.11.0"                 % "test, it",
-    "de.flapdoodle.embed"     %  "de.flapdoodle.embed.mongo"  % "3.5.1"                 % Test,
+    "de.flapdoodle.embed"     %  "de.flapdoodle.embed.mongo"  % "3.5.3"                 % Test,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.73.0"                % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.62.2"                % "test, it",
     "com.github.tomakehurst"  %  "wiremock-jre8"              % "2.35.0"                % Test,
