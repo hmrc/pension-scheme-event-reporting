9ce40e4fb19201261e55fee78d3f96204c970df8
ngmjohnstone <ngmjohnstone@yahoo.com>
2023-04-24 12:13:15 +0100

    PODS-8194: WIP

--- app/transformations/ETMPToFrontEnd/EventOneReport.scala
+++ app/transformations/ETMPToFrontEnd/EventOneReport.scala
@@ -18,40 +18,82 @@ package transformations.ETMPToFrontEnd
 
 import play.api.libs.json._
 import transformations.Transformer
+import play.api.libs.functional.syntax._
+import play.api.libs.json.Reads._
 
 //noinspection ScalaStyle
-object EventOneReport extends Transformer {
+object EventOneReport {
 
   import Paths._
   import ReadsUtilities._
 
   
   implicit val rds1833Api: Reads[JsObject] =
-    dummyUAPath.json.copyFrom(pathEtmpEventDetails.read(readsMembers))
+    dummyPath.json.copyFrom(pathEtmpEvent1Details.read(readsEvent1Details))
 
-  private lazy val readsMembers: Reads[JsArray] = __.read(Reads.seq(rdsFirstName).map(JsArray(_)))
-
+  private lazy val readsEvent1Details: Reads[JsArray] = __.read(Reads.seq(
+        (readsMemberType and readsIndividualMemberDetails).reduce
+      ).map(JsArray(_)))
 }
 
 private object ReadsUtilities extends Transformer {
 
   import Paths._
 
-  // Dummy test reads
-  implicit val rdsFirstName: Reads[JsObject] = {(
-    dummyUAPath.json.copyFrom(pathEtmpIndividualMemberDetailsFirstName.json.pick)
-    )}
+  val readsMemberType: Reads[JsObject] = {
+    pathUAMemberType.json.copyFrom(pathEtmpMemberType.json.pick)
+  }
 
+  val readsIndividualOrEmployerMemberDetails: Unit = {
+    // TODO: implement.
+  }
+
+  val readsIndividualMemberDetails: Reads[JsObject] = {(
+    pathUAIndividualMemberDetailsFirstName.json.copyFrom(pathEtmpIndividualMemberDetailsFirstName.json.pick) and
+      pathUAIndividualMemberDetailsLastName.json.copyFrom(pathEtmpIndividualMemberDetailsLastName.json.pick) and
+      pathUAIndividualMemberDetailsNino.json.copyFrom(pathEtmpIndividualMemberDetailsNino.json.pick) and
+      pathUAIndividualMemberDetailsSignedMandate.json.copyFrom(pathEtmpIndividualMemberDetailsSignedMandate.json.pick) and
+      pathUAIndividualMemberDetailsPmtMoreThan25PerFundValue.json.copyFrom(pathEtmpIndividualMemberDetailsPmtMoreThan25PerFundValue.json.pick) and
+      pathUAIndividualMemberDetailsSchemePayingSurcharge.json.copyFrom(pathEtmpIndividualMemberDetailsSchemePayingSurcharge.json.pick)
+    ).reduce
+  }
+
+  // TODO: incorrect implementation.
+  val readsEmployerMemberDetails: Reads[JsObject] = {(
+    pathUAIndividualMemberDetailsFirstName.json.copyFrom(pathEtmpIndividualMemberDetailsFirstName.json.pick) and
+      pathUAIndividualMemberDetailsLastName.json.copyFrom(pathEtmpIndividualMemberDetailsLastName.json.pick) and
+      pathUAIndividualMemberDetailsNino.json.copyFrom(pathEtmpIndividualMemberDetailsNino.json.pick) and
+      pathUAIndividualMemberDetailsSignedMandate.json.copyFrom(pathEtmpIndividualMemberDetailsSignedMandate.json.pick) and
+      pathUAIndividualMemberDetailsPmtMoreThan25PerFundValue.json.copyFrom(pathEtmpIndividualMemberDetailsPmtMoreThan25PerFundValue.json.pick) and
+      pathUAIndividualMemberDetailsSchemePayingSurcharge.json.copyFrom(pathEtmpIndividualMemberDetailsSchemePayingSurcharge.json.pick)
+    ).reduce
+  }
 }
 
 private object Paths {
 
   /* UserAnswers */
-  val dummyUAPath: JsPath = __ \ "dummy"
+  // TODO: these will need to be amended to match the actual UA structure.
+  val dummyPath: JsPath = __ \ Symbol("dummy")
+  val pathUAMemberType: JsPath = __ \ Symbol("memberType")
+  val pathUAIndividualMemberDetailsFirstName: JsPath = __ \ Symbol("individualMemberDetails")  \ Symbol("firstName")
+  val pathUAIndividualMemberDetailsLastName: JsPath = __ \ Symbol("individualMemberDetails")  \ Symbol("lastName")
+  val pathUAIndividualMemberDetailsNino: JsPath = __ \ Symbol("individualMemberDetails")  \ Symbol("lastName")
+  val pathUAIndividualMemberDetailsSignedMandate: JsPath = __ \ Symbol("individualMemberDetails")  \ Symbol("signedMandate")
+  val pathUAIndividualMemberDetailsPmtMoreThan25PerFundValue: JsPath = __ \ Symbol("individualMemberDetails")  \ Symbol("pmtMoreThan25PerFundValue")
+  val pathUAIndividualMemberDetailsSchemePayingSurcharge: JsPath = __ \ Symbol("individualMemberDetails")  \ Symbol("schemePayingSurcharge")
 
 
   /* ETMP */
-  val pathEtmpEventDetails: JsPath = __ \ "event1Details"
+  val pathEtmpEvent1Details: JsPath = __ \ "event1Details"
+
+  // ETMP - relative paths from "event1Details"
+  val pathEtmpMemberType: JsPath = __ \ Symbol("memberType")
   val pathEtmpIndividualMemberDetailsFirstName: JsPath = __ \ Symbol("individualMemberDetails")  \ Symbol("firstName")
+  val pathEtmpIndividualMemberDetailsLastName: JsPath = __ \ Symbol("individualMemberDetails")  \ Symbol("lastName")
+  val pathEtmpIndividualMemberDetailsNino: JsPath = __ \ Symbol("individualMemberDetails")  \ Symbol("lastName")
+  val pathEtmpIndividualMemberDetailsSignedMandate: JsPath = __ \ Symbol("individualMemberDetails")  \ Symbol("signedMandate")
+  val pathEtmpIndividualMemberDetailsPmtMoreThan25PerFundValue: JsPath = __ \ Symbol("individualMemberDetails")  \ Symbol("pmtMoreThan25PerFundValue")
+  val pathEtmpIndividualMemberDetailsSchemePayingSurcharge: JsPath = __ \ Symbol("individualMemberDetails")  \ Symbol("schemePayingSurcharge")
 }
 
