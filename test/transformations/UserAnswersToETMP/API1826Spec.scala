a40e14088600faa7965fc1b2dee0d77f2f27288d
Shehzad Ismail <shehzad.ismail@atos.net>
2023-05-25 14:49:53 +0100

    [PODS-8395: Removed printlns]

--- test/transformations/UserAnswersToETMP/API1826Spec.scala
+++ test/transformations/UserAnswersToETMP/API1826Spec.scala
@@ -66,11 +66,6 @@ class API1826Spec extends AnyFreeSpec with Matchers
         case (userAnswers: JsObject, expectedResponse: JsObject) =>
           val result = userAnswers.validate(API1826.transformToETMPData)
           val expectedResult = JsSuccess(expectedResponse)
-
-          println(s"\n\n UserAnswers ======== ${userAnswers}\n\n")
-          println(s"\n\n expectedResult ======== ${expectedResult}\n\n")
-          println(s"\n\n result ======== ${result}\n\n")
-
           result mustBe expectedResult
       }
     }
