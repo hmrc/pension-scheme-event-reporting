32b7f85c70341393b9aaef36aa7aebd177c2c4d8
brownbonnie <bonnie.brown@digital.hmrc.gov.uk>
2023-05-25 14:03:23 +0100

    PODS-8339: WIP

---
+++ app/controllers/cache/ParsingAndValidationOutcomeController.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.cache
+
+import com.google.inject.Inject
+import play.api.Logger
+import play.api.mvc._
+import repositories.ParsingAndValidationOutcomeRepository
+import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals
+import uk.gov.hmrc.auth.core.{AuthConnector, AuthorisedFunctions, Enrolment}
+import uk.gov.hmrc.http.{HeaderCarrier, UnauthorizedException}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class ParsingAndValidationOutcomeController @Inject()(
+                                             repository: ParsingAndValidationOutcomeRepository,
+                                             val authConnector: AuthConnector,
+                                             cc: ControllerComponents
+                                           )(implicit ec: ExecutionContext) extends BackendController(cc) with AuthorisedFunctions {
+
+  import ParsingAndValidationOutcomeController._
+
+  private val logger = Logger(classOf[ParsingAndValidationOutcomeController])
+
+  def post: Action[AnyContent] = Action.async {
+    implicit request =>
+      getId { id =>
+        request.body.asJson.map {
+          jsValue =>
+            repository.save(id, jsValue)
+              .map(_ => Created)
+        } getOrElse Future.successful(BadRequest)
+      }
+  }
+
+  def get: Action[AnyContent] = Action.async {
+    implicit request =>
+      getId { id =>
+        repository.get(id).map { response =>
+          logger.debug(message = s"ParsingAndValidationOutcomeController.get: Response for request Id $id is $response")
+          response.map {
+            Ok(_)
+          } getOrElse NotFound
+        }
+      }
+  }
+
+  def delete: Action[AnyContent] = Action.async {
+    implicit request =>
+      getId { id =>
+        repository.remove(id).map(_ => Ok)
+      }
+  }
+
+  private def getId(block: String => Future[Result])
+                   (implicit hc: HeaderCarrier): Future[Result] = {
+    authorised(Enrolment("HMRC-PODS-ORG") or Enrolment("HMRC-PODSPP-ORG")).retrieve(Retrievals.externalId) {
+      case Some(id) => block(id)
+      case _ => Future.failed(IdNotFoundFromAuth())
+    }
+  }
+}
+
+object ParsingAndValidationOutcomeController {
+
+  case class IdNotFoundFromAuth() extends UnauthorizedException("Not Authorised - Unable to retrieve id")
+
+}
+
+
+
+
---
+++ test/controllers/cache/ParsingAndValidationOutcomeControllerSpec.scala
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.cache
+
+import akka.util.ByteString
+import controllers.cache.ParsingAndValidationOutcomeController.IdNotFoundFromAuth
+import org.apache.commons.lang3.RandomUtils
+import org.mockito.ArgumentMatchers.{eq => eqTo, _}
+import org.mockito.Mockito._
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AsyncWordSpec
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.Application
+import play.api.inject.bind
+import play.api.inject.guice.{GuiceApplicationBuilder, GuiceableModule}
+import play.api.libs.json.Json
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import repositories.ParsingAndValidationOutcomeRepository
+import uk.gov.hmrc.auth.core.AuthConnector
+
+import scala.concurrent.Future
+
+class ParsingAndValidationOutcomeControllerSpec extends AsyncWordSpec with Matchers with MockitoSugar with BeforeAndAfterEach { // scalastyle:off magic.number
+
+  private val repo = mock[ParsingAndValidationOutcomeRepository]
+  private val authConnector: AuthConnector = mock[AuthConnector]
+  private val id = "id"
+  private val fakePostRequest = FakeRequest("POST", "/")
+  private val fakeRequest = FakeRequest()
+
+  def modules: Seq[GuiceableModule] = {
+    Seq(
+      bind[AuthConnector].toInstance(authConnector),
+      bind[ParsingAndValidationOutcomeRepository].toInstance(repo),
+    )
+  }
+
+  private val application: Application = new GuiceApplicationBuilder()
+    .configure(conf = "auditing.enabled" -> false, "metrics.enabled" -> false, "metrics.jvm" -> false).
+    overrides(modules: _*).build()
+
+  val controller: ParsingAndValidationOutcomeController = application.injector.instanceOf[ParsingAndValidationOutcomeController]
+
+  override def beforeEach(): Unit = {
+    reset(repo)
+    reset(authConnector)
+    super.beforeEach()
+  }
+
+  "ParsingAndValidationOutcome Controller" when {
+    "calling get" must {
+      "return OK with the data" in {
+        when(repo.get(eqTo(id))(any())) thenReturn Future.successful(Some(Json.obj("testId" -> "data")))
+        when(authConnector.authorise[Option[String]](any(), any())(any(), any())) thenReturn Future.successful(Some(id))
+
+        val result = controller.get(fakeRequest)
+        status(result) mustEqual OK
+        contentAsJson(result) mustEqual Json.obj(fields = "testId" -> "data")
+      }
+
+      "return NOT FOUND when the data doesn't exist" in {
+        when(repo.get(eqTo(id))(any())) thenReturn Future.successful(None)
+        when(authConnector.authorise[Option[String]](any(), any())(any(), any())) thenReturn Future.successful(Some(id))
+
+        val result = controller.get(fakeRequest)
+        status(result) mustEqual NOT_FOUND
+      }
+
+      "throw an exception when the repository call fails" in {
+        when(repo.get(eqTo(id))(any())) thenReturn Future.failed(new Exception())
+        when(authConnector.authorise[Option[String]](any(), any())(any(), any())) thenReturn Future.successful(Some(id))
+        val result = controller.get(fakeRequest)
+        an[Exception] must be thrownBy status(result)
+      }
+
+      "throw an exception when the call is not authorised" in {
+        when(authConnector.authorise[Option[String]](any(), any())(any(), any())) thenReturn Future.successful(None)
+
+        val result = controller.get(fakeRequest)
+        an[IdNotFoundFromAuth] must be thrownBy status(result)
+      }
+
+    }
+
+    "calling save" must {
+
+      "return OK when the data is saved successfully" in {
+        when(repo.save(any(), any())(any())) thenReturn Future.successful((): Unit)
+        when(authConnector.authorise[Option[String]](any(), any())(any(), any())) thenReturn Future.successful(Some(id))
+
+        val result = controller.post(fakePostRequest.withJsonBody(Json.obj("value" -> "data")))
+        status(result) mustEqual CREATED
+      }
+
+      "return BAD REQUEST when the request body cannot be parsed" in {
+        when(repo.save(any(), any())(any())) thenReturn Future.successful((): Unit)
+        when(authConnector.authorise[Option[String]](any(), any())(any(), any())) thenReturn Future.successful(Some(id))
+
+        val result = controller.post(fakePostRequest.withRawBody(ByteString(RandomUtils.nextBytes(512001))))
+        status(result) mustEqual BAD_REQUEST
+      }
+
+      "throw an exception when the call is not authorised" in {
+        when(authConnector.authorise[Option[String]](any(), any())(any(), any())) thenReturn Future.successful(None)
+
+        val result = controller.post(fakePostRequest.withJsonBody(Json.obj(fields = "value" -> "data")))
+        an[IdNotFoundFromAuth] must be thrownBy status(result)
+      }
+    }
+
+    "calling delete" must {
+      "return OK when the data is removed successfully" in {
+        when(repo.remove(eqTo(id))(any())) thenReturn Future.successful(true)
+        when(authConnector.authorise[Option[String]](any(), any())(any(), any())) thenReturn Future.successful(Some(id))
+
+        val result = controller.delete(fakeRequest)
+        status(result) mustEqual OK
+      }
+
+      "throw an exception when the call is not authorised" in {
+        when(authConnector.authorise[Option[String]](any(), any())(any(), any())) thenReturn Future.successful(None)
+
+        val result = controller.delete(fakeRequest)
+        an[IdNotFoundFromAuth] must be thrownBy status(result)
+      }
+    }
+  }
+}
