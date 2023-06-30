565941f64327c951ab96182920ccbfb793c0781e
grantrobson <grant.robson@digital.hmrc.gov.uk>
2023-06-30 14:55:34 +0100

    PODS-8529 WIP

--- app/transformations/ETMPToFrontEnd/API1834.scala
+++ app/transformations/ETMPToFrontEnd/API1834.scala
@@ -17,7 +17,7 @@
 package transformations.ETMPToFrontEnd
 
 import models.enumeration.EventType
-import models.enumeration.EventType.Event10
+import models.enumeration.EventType.{Event10, Event11}
 import play.api.libs.functional.syntax._
 import play.api.libs.json.Reads._
 import play.api.libs.json._
@@ -30,45 +30,43 @@ object API1834 {
   implicit def reads(eventType: EventType): Reads[JsObject] = {
     eventType match {
       case Event10 => event10Reads
+      case Event11 => event11Reads
       case e => Reads.failed(s"Unknown event type $e")
     }
   }
 
   /*
   "eventDetails" -> Json.obj(
-    "event10" -> Json.arr(
-      Json.obj(
-        "recordVersion" -> "001",
-        "invRegScheme" -> Json.obj(
-          "startDateDetails" -> Json.obj(
-            "startDateOfInvReg" -> date,
-            "contractsOrPolicies" -> toYesNo(boolean)
-          )
-        )
-      )
-   */
-
-  /* UA:
-  val expected = Json.obj(
-    "event10" -> Json.obj(
-      "becomeOrCeaseScheme" -> "itBecameAnInvestmentRegulatedPensionScheme",
-      "schemeChangeDate" -> Json.obj(
-        "schemeChangeDate" -> date
-      ),
-      "contractsOrPolicies" -> boolean
+    "event11" -> Json.obj(
+      "recordVersion" -> "001",
+      "unauthorisedPmtsDate" -> date1,
+      "contractsOrPoliciesDate" -> date2
     )
   )
+)
+
+val expected = Json.obj(
+  "event11" -> Json.obj(
+    "hasSchemeChangedRulesUnAuthPayments" -> true,
+    "unAuthPaymentsRuleChangeDate" -> Json.obj(
+      "date" -> date1
+    ),
+    "hasSchemeChangedRulesInvestmentsInAssets" -> true,
+    "investmentsInAssetsRuleChangeDate" -> Json.obj {
+      "date" -> date2
+    }
+  )
+)
    */
 
-
-  private val yesNoTransform: String => JsBoolean = {
-    case "Yes" => JsBoolean(true)
-    case "No" => JsBoolean(false)
-    case _ => JsBoolean(false)
+  private val event11Reads: Reads[JsObject] = {
+    (__ \ "event11" \ "hasSchemeChangedRulesUnAuthPayments")
+    //(__ \ "eventDetails" \ "event11")
   }
 
-  private val event10ItemReads: Reads[JsObject] = {
-    (
+
+  private val event10Reads: Reads[JsObject] = {
+    val event10ItemReads: Reads[JsObject] = {
       (
         (__ \ "invRegScheme" \ "startDateDetails" \ "startDateOfInvReg").readNullable[String] and
           (__ \ "invRegScheme" \ "startDateDetails" \ "contractsOrPolicies").readNullable[String] and
@@ -87,14 +85,11 @@ object API1834 {
                 (__ \ "becomeOrCeaseScheme").json.put(JsString("itHasCeasedToBeAnInvestmentRegulatedPensionScheme")) and
                   (__ \ "schemeChangeDate" \ "schemeChangeDate").json.put(JsString(cd))
                 ).reduce
-            case (a,b,c) => Reads.failed[JsObject](s"Invalid $a $b $c")
+            case (a, b, c) => Reads.failed[JsObject](s"Invalid $a $b $c")
           }
         }
-      )
       ).flatMap(identity)
-  }
-
-  private val event10Reads: Reads[JsObject] = {
+    }
     (__ \ "eventDetails" \ "event10").read(Reads.seq(event10ItemReads)).map { s =>
       s.headOption match {
         case None => Json.obj()
@@ -106,5 +101,9 @@ object API1834 {
     }
   }
 
-
+  private val yesNoTransform: String => JsBoolean = {
+    case "Yes" => JsBoolean(true)
+    case "No" => JsBoolean(false)
+    case _ => JsBoolean(false)
+  }
 }
