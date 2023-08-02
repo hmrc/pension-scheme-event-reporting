a1e178f023cb54656af689fe39763fcf6bfde961
grantrobson <grant.robson@digital.hmrc.gov.uk>
2023-08-02 07:37:50 +0100

    PODS-8634 Add correct schema and uncomment transformation code

--- app/transformations/UserAnswersToETMP/API1826.scala
+++ app/transformations/UserAnswersToETMP/API1826.scala
@@ -280,7 +280,7 @@ object API1826 extends Transformer {
       ev18 <- deleteEventTransform(EventType.Event18,event18Reads)
       ev19 <- deleteEventTransform(EventType.Event19,event19Reads)
       ev20 <- deleteEventTransform(EventType.Event20,event20Reads)
-      schWindUp <- schemeWindUpReads(false) //(delete && deleteEvent == EventType.WindUp) TODO: Hardcoded to false because regex validation does not pass
+      schWindUp <- schemeWindUpReads(delete && deleteEvent == EventType.WindUp)
       header <- HeaderForAllAPIs.transformToETMPData()
     } yield {
       header ++ eventTypeNodes((ev10 ++ ev11 ++ ev12 ++ ev13 ++ ev14 ++ ev18 ++ ev19 ++ ev20 ++ schWindUp).toSeq)
