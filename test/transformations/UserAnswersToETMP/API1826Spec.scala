9352f9d1a34f0e4051fcd708dd75708b96ecc1ba
grantrobson <grant.robson@digital.hmrc.gov.uk>
2023-08-07 08:25:55 +0100

    PODS-8517 Remove commented code no longer needed

--- test/transformations/UserAnswersToETMP/API1826Spec.scala
+++ test/transformations/UserAnswersToETMP/API1826Spec.scala
@@ -120,50 +120,22 @@ class API1826Spec extends AnyFreeSpec with Matchers
       }
     }
   }
-//
-//  //   TODO: Haven't had time to fix the test below which fails due to different versions being generated. This is a test problem not code issue
-//      "must transform all events when present" in {
-//        val ev10 = generateUserAnswersAndPOSTBodyEvent10.sample.get
-//        val ev11 = generateUserAnswersAndPOSTBodyEvent11.sample.get
-//        val ev12 = generateUserAnswersAndPOSTBodyEvent12.sample.get
-//        val ev13 = generateUserAnswersAndPOSTBodyEvent13.sample.get
-//        val ev14 = generateUserAnswersAndPOSTBodyEvent14.sample.get
-//        val ev18 = generateUserAnswersAndPOSTBodyEvent18.sample.get
-//        val ev20 = generateUserAnswersAndPOSTBodyEvent20.sample.get
-//        val windUp = generateUserAnswersAndPOSTBodyWindUp.sample.get
-//
-//        val ua = ev10._1 ++ ev11._1 ++ ev12._1 ++ ev13._1 ++ ev14._1 ++ ev18._1 ++ ev20._1 ++ windUp._1
-//        val result = ua.validate(API1826.transformToETMPData)
-//
-//  println("\nFULL UA" + ua)
-//        println( "\n>>>ev 11 ua:" + ev11._1)
-//        println( "\n>>>ev 11 exp:" + ev11._2)
-//
-//  //      checkExpectation("event10", result, ev10._2)
-//        checkExpectation("event11", result, ev11._2)
-//  //      checkExpectation("event12", result, ev12._2)
-//  //      checkExpectation("event13", result, ev13._2)
-//  //      checkExpectation("event14", result, ev14._2)
-//  //      checkExpectation("event18", result, ev18._2)
-//  //      checkExpectation("event20", result, ev20._2)
-//  //      checkExpectation("eventWindup", result, windUp._2)
-//      }
 
-    "must not transform an event that is not present" in {
-      val userAnswers: JsObject =
-        Json.obj("taxYear" -> "2020")
+  "must not transform an event that is not present" in {
+    val userAnswers: JsObject =
+      Json.obj("taxYear" -> "2020")
 
-      val result = userAnswers.validate(API1826.transformToETMPData(EventType.Event10, delete = false))
-      val expected: JsObject = {
-        Json.obj(
-          "eventReportDetails" -> Json.obj(
-            "reportStartDate" -> "2020-04-06",
-            "reportEndDate" -> "2021-04-05"
-          )
+    val result = userAnswers.validate(API1826.transformToETMPData(EventType.Event10, delete = false))
+    val expected: JsObject = {
+      Json.obj(
+        "eventReportDetails" -> Json.obj(
+          "reportStartDate" -> "2020-04-06",
+          "reportEndDate" -> "2021-04-05"
         )
-      }
-      result.asOpt mustBe Some(expected)
+      )
     }
+    result.asOpt mustBe Some(expected)
+  }
 
 }
 
