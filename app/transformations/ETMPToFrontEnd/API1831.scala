1fa6d2824751e273094e066b593469f14d831198
LauraToddCodes <74312582+LauraToddCodes@users.noreply.github.com>
2024-07-15 10:44:18 +0100

    PODS-8479 - Revert API1831 changes

--- app/transformations/ETMPToFrontEnd/API1831.scala
+++ app/transformations/ETMPToFrontEnd/API1831.scala
@@ -29,64 +29,52 @@ object API1831 {
 
   implicit val rds1831Api: Reads[JsObject] = (
     reqReads(uaPstr, etmpPstr)                                                    and
-    reqReads(uaReportStartDate, etmpReportStartDate)                              and
-    reqReads(uaReportEndDate, etmpReportEndDate)                                  and
-    reqReads(uaSubmittedBy, etmpSubmittedBy)                                      and
-    reqReads(uaSubmittedID, etmpSubmittedID)                                      and
-    optReads(uaSchemeMasterTrustStartDate, etmpSchemeMasterTrustDetailsStartDate) and
-    optReads(uaSchemeMasterTrustCeaseDate, etmpSchemeMasterTrustDetailsCeaseDate) and
-    optReads(uaPsaDeclaration1, etmpPsaDeclaration1)                              and
-    optReads(uaPsaDeclaration2, etmpPsaDeclaration2)                              and
-    optReads(uaAuthorisedPSAID, etmpAuthorisedPSAID)                              and
-    optReads(uaPspDeclaration1, etmpPspDeclaration1)                              and
-    optReads(uaPspDeclaration2, etmpPspDeclaration2)
+      reqReads(uaReportStartDate, etmpReportStartDate)                              and
+      reqReads(uaReportEndDate, etmpReportEndDate)                                  and
+      reqReads(uaSubmittedBy, etmpSubmittedBy)                                      and
+      reqReads(uaSubmittedID, etmpSubmittedID)                                      and
+      optReads(uaSchemeMasterTrustStartDate, etmpSchemeMasterTrustDetailsStartDate) and
+      optReads(uaSchemeMasterTrustCeaseDate, etmpSchemeMasterTrustDetailsCeaseDate) and
+      optReads(uaPsaDeclaration1, etmpPsaDeclaration1)                              and
+      optReads(uaPsaDeclaration2, etmpPsaDeclaration2)                              and
+      optReads(uaAuthorisedPSAID, etmpAuthorisedPSAID)                              and
+      optReads(uaPspDeclaration1, etmpPspDeclaration1)                              and
+      optReads(uaPspDeclaration2, etmpPspDeclaration2)
     ).reduce
+}
+
+private object API1831Paths {
+  // UA
+  val uaPstr:                                 JsPath = __ \ "pstr"
+  val uaReportStartDate:                      JsPath = __ \ "reportStartDate"
+  val uaReportEndDate:                        JsPath = __ \ "reportEndDate"
+  val uaSubmittedBy:                          JsPath = __ \ "submittedBy"
+  val uaSubmittedID:                          JsPath = __ \ "submittedID"
+  val uaSchemeMasterTrustStartDate:           JsPath = __ \ "schemeMasterTrustStartDate"
+  val uaSchemeMasterTrustCeaseDate:           JsPath = __ \ "schemeMasterTrustCeaseDate"
+  val uaPsaDeclaration1:                      JsPath = __ \ "psaDeclaration1"
+  val uaPsaDeclaration2:                      JsPath = __ \ "psaDeclaration2"
+  val uaAuthorisedPSAID:                      JsPath = __ \ "authorisedPSAID"
+  val uaPspDeclaration1:                      JsPath = __ \ "pspDeclaration1"
+  val uaPspDeclaration2:                      JsPath = __ \ "pspDeclaration2"
+
+  // ETMP
+  val etmpPstr:                               JsPath = __ \ "schemeDetails" \ "pSTR"
+  val etmpReportStartDate:                    JsPath = __ \ "er20aDetails" \ "reportStartDate"
+  val etmpReportEndDate:                      JsPath = __ \ "er20aDetails" \ "reportEndDate"
+  val etmpSubmittedBy:                        JsPath = __ \ "erDeclarationDetails" \ "submittedBy"
+  val etmpSubmittedID:                        JsPath = __ \ "erDeclarationDetails" \ "submittedID"
+  val etmpSchemeMasterTrustDetailsStartDate:  JsPath = __ \ "schemeMasterTrustDetails" \ "startDate"
+  val etmpSchemeMasterTrustDetailsCeaseDate:  JsPath = __ \ "schemeMasterTrustDetails" \ "ceaseDate"
+  val etmpPsaDeclaration1:                    JsPath = __ \ "erDeclarationDetails" \ "psaDeclaration" \ "psaDeclaration1"
+  val etmpPsaDeclaration2:                    JsPath = __ \ "erDeclarationDetails" \ "psaDeclaration" \ "psaDeclaration2"
+  val etmpAuthorisedPSAID:                    JsPath = __ \ "erDeclarationDetails" \ "pspDeclaration" \ "authorisedPSAID"
+  val etmpPspDeclaration1:                    JsPath = __ \ "erDeclarationDetails" \ "pspDeclaration" \ "pspDeclaration1"
+  val etmpPspDeclaration2:                    JsPath = __ \ "erDeclarationDetails" \ "pspDeclaration" \ "pspDeclaration2"
 }
 
 private object Event20AReadsUtilities extends Transformer {
   lazy val reqReads: (JsPath, JsPath) => Reads[JsObject] = (uaPath: JsPath, etmpPath: JsPath) => uaPath.json.copyFrom(etmpPath.json.pick)
 
   lazy val optReads: (JsPath, JsPath) => Reads[JsObject] = (uaPath: JsPath, etmpPath: JsPath) => uaPath.json.copyFrom(etmpPath.json.pick).orElse(doNothing)
-}
-
-private object API1831Paths {
-  // UA
-  val uaAuthorisedPSAID:                      JsPath = __ \ "authorisedPSAID"
-  val uaPsaDeclaration1:                      JsPath = __ \ "psaDeclaration1"
-  val uaPsaDeclaration2:                      JsPath = __ \ "psaDeclaration2"
-  val uaPspDeclaration1:                      JsPath = __ \ "pspDeclaration1"
-  val uaPspDeclaration2:                      JsPath = __ \ "pspDeclaration2"
-  val uaPstr:                                 JsPath = __ \ "pstr"
-  val uaReportStartDate:                      JsPath = __ \ "reportStartDate"
-  val uaReportEndDate:                        JsPath = __ \ "reportEndDate"
-  val uaSchemeMasterTrustStartDate:           JsPath = __ \ "schemeMasterTrustStartDate"
-  val uaSchemeMasterTrustCeaseDate:           JsPath = __ \ "schemeMasterTrustCeaseDate"
-  val uaSubmittedBy:                          JsPath = __ \ "submittedBy"
-  val uaSubmittedID:                          JsPath = __ \ "submittedID"
-
-  // ETMP
-  private val etmpEr20aDetails:               JsPath = __ \ "er20aDetails"
-  val etmpReportStartDate:                    JsPath = etmpEr20aDetails \ "reportStartDate"
-  val etmpReportEndDate:                      JsPath = etmpEr20aDetails \ "reportEndDate"
-
-  private val etmpErDeclarationDetails:       JsPath = __ \ "erDeclarationDetails"
-  val etmpSubmittedBy:                        JsPath = etmpErDeclarationDetails \ "submittedBy"
-  val etmpSubmittedID:                        JsPath = etmpErDeclarationDetails \ "submittedID"
-
-  private val etmpPsaDeclaration:             JsPath = __ \ "erDeclarationDetails" \ "psaDeclaration"
-  val etmpPsaDeclaration1:                    JsPath = etmpPsaDeclaration \ "psaDeclaration1"
-  val etmpPsaDeclaration2:                    JsPath = etmpPsaDeclaration \ "psaDeclaration2"
-
-  private val etmpPspDeclaration:             JsPath = __ \ "erDeclarationDetails" \ "pspDeclaration"
-  val etmpAuthorisedPSAID:                    JsPath = etmpPspDeclaration \ "authorisedPSAID"
-  val etmpPspDeclaration1:                    JsPath = etmpPspDeclaration \ "pspDeclaration1"
-  val etmpPspDeclaration2:                    JsPath = etmpPspDeclaration \ "pspDeclaration2"
-
-  val etmpPstr:                               JsPath = __ \ "schemeDetails" \ "pSTR"
-
-  private val etmpSchemeMasterTrustDetails:   JsPath = __ \ "schemeMasterTrustDetails"
-  val etmpSchemeMasterTrustDetailsStartDate:  JsPath = etmpSchemeMasterTrustDetails \ "startDate"
-  val etmpSchemeMasterTrustDetailsCeaseDate:  JsPath = etmpSchemeMasterTrustDetails \ "ceaseDate"
-
-
 }
