ed088a863e1fe3f06ab198d75cda9f93d06bbffc
nonebula <147166151+nonebula@users.noreply.github.com>
2024-10-21 10:24:33 +0100

    Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,23 +2,23 @@ import sbt._
 
 object AppDependencies {
 
-  private val mongoVersion = "1.7.0"
-  private val bootstrapVersion = "8.4.0"
+  private val mongoVersion = "2.2.0"
+  private val bootstrapVersion = "9.5.0"
   val compile = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % mongoVersion,
     "com.github.java-json-tools"    %  "json-schema-validator"      % "2.2.14",
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.16.1"
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.17.2"
   )
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion                 % "test, it",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % mongoVersion           % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.64.0"                % "test, it",
-    "org.scalatest"           %% "scalatest"                  % "3.2.15"                % Test,
-    "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0"                 % Test,
+    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.64.8"                % "test, it",
+    "org.scalatest"           %% "scalatest"                  % "3.2.19"                % Test,
+    "org.scalatestplus.play"  %% "scalatestplus-play"         % "7.0.1"                 % Test,
     "org.scalatestplus"       %% "mockito-4-6"                % "3.2.15.0"              % Test,
-    "org.scalatestplus"       %% "scalacheck-1-17"            % "3.2.15.0"              % Test
+    "org.scalatestplus"       %% "scalacheck-1-17"            % "3.2.18.0"              % Test
   )
 
   def apply(): Seq[ModuleID] = compile ++ test
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,6 +4,6 @@ resolvers += Resolver.typesafeRepo("releases")
 
 addSbtPlugin("uk.gov.hmrc"       % "sbt-auto-build"     % "3.22.0")
 addSbtPlugin("uk.gov.hmrc"       % "sbt-distributables" % "2.5.0")
-addSbtPlugin("org.playframework" % "sbt-plugin"         % "3.0.1")
+addSbtPlugin("org.playframework" % "sbt-plugin"         % "3.0.5")
 addSbtPlugin("org.scoverage"     % "sbt-scoverage"      % "2.0.9")
 addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0" exclude("org.scala-lang.modules", "scala-xml_2.12"))
