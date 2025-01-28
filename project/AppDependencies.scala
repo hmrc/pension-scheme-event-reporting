32684ee3ea6c02ba43db92f81ea0bcb69418dbb3
Pavel Vjalicin <pavelvjalicin@users.noreply.github.com>
2025-01-28 13:06:30 +0000

    PODS-10333: bumped version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
 
   private val mongoVersion = "2.2.0"
-  private val bootstrapVersion = "9.5.0"
+  private val bootstrapVersion = "9.7.0"
   val compile = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % mongoVersion,
