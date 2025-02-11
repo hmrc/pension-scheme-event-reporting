8f97fd31a806162e8149fdf32480f546860365c2
Pavel Vjalicin <pavelvjalicin@users.noreply.github.com>
2025-02-11 14:37:38 +0000

    PODS-10383: Introduced loggedInAs check for cases with both enrolments

---
+++ app/connectors/SessionDataCacheConnector.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import com.google.inject.Inject
+import config.AppConfig
+import play.api.http.Status._
+import play.api.libs.json._
+import uk.gov.hmrc.http.HttpReads.Implicits._
+import uk.gov.hmrc.http._
+import uk.gov.hmrc.http.client.HttpClientV2
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class SessionDataCacheConnector @Inject()(
+                                           config: AppConfig,
+                                           http: HttpClientV2
+                                         ) {
+  private def url() = url"${config.pensionsAdministratorUrl}/pension-administrator/journey-cache/session-data-self"
+
+  def fetch()(implicit ec: ExecutionContext, headerCarrier: HeaderCarrier): Future[Option[JsValue]] = {
+
+    http.get(url()).execute[HttpResponse]
+      .recoverWith(mapExceptionsToStatus)
+      .map { response =>
+        response.status match {
+          case NOT_FOUND =>
+            None
+          case OK =>
+            Some(Json.parse(response.body))
+          case _ =>
+            throw new HttpException(response.body, response.status)
+        }
+      }
+  }
+
+  private def mapExceptionsToStatus: PartialFunction[Throwable, Future[HttpResponse]] = {
+    case _: NotFoundException =>
+      Future.successful(HttpResponse(NOT_FOUND, "Not found"))
+  }
+}
